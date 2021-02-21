e9e02ff33c93329deffcc91e2aa60c17037f0811
Malcolm Sparks <mal@juxt.pro>
2021-02-21 09:01:57 +0000

    checkpoint

--- style/tailwind.config.js
+++ style/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    // We cannot purge as we don't know what HTML files are in the database
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
